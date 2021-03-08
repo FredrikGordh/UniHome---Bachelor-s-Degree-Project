9a6e259fdcdc336c7efee4deebb7761542e2d226
albinjanzen <albinjanzen@gmail.com>
2021-03-08 15:50:35 +0100

    server start code

--- server/main.py
+++ server/main.py
@@ -1,1 +1,21 @@
-test
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+dbConnection = "mysql+pymysql://{}:{}@{}/{}".format(
+    'client', 'tddd83', '83.249.161.212', 'unihome')
+
+app = Flask(__name__, static_folder='../client', static_url_path='/')
+app.config['SQLALCHEMY_DATABASE_URI'] = dbConnection
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'LuSg31rsf76nGvMVjzeqV1R0vchtnxu6XTrhrOSLtek'
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def default():
+    return "404"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
