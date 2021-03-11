d2f2322ce947687ede7d6f6cc032361367e69aa6
albinjanzen <albinjanzen@gmail.com>
2021-03-11 15:22:13 +0100

    added searchbar on home and edited the Mustache implementation

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-    $("#content").html(Mustache.render(home));
-    $("#header").html(Mustache.render(home_header))
+    $("#content").html($("#home").html());
+    $("#header").html($("#home_header").html());
 })
--- client/templates.js
+++ client/templates.js
@@ -1,26 +1,3 @@
-var home = '<div id="home-now-hot" class="section">'
-    + '<h1> Hetaste boendena/Bäst reviews</h1>'
-    + '</div>'
-    + '<div id="home-areas" class="section">'
-    + '<h1>Områden</h1>'
-    + '</div>'
-    + '<div id="home-types" class="section">'
-    + '<h1>Typer av boenden</h1>'
-    + '</div>'
-    + '<div id="home-reviews" class="section">'
-    + '<h1>Reviews</h1>'
-    + '</div>';
-
-var search = '<div id="searchbox"></div>'
-    + '<div id="searchresult"></div>';
-
-var home_header = '<div class="container">'
-    + '<h1>Header</h1>'
-    + '<div id="home_search">'
-    + '<div class="row">'
-    + '<div class="col"></div'
-    + '<div class="col"></div'
-    + '<div class="col"></div'
-    + '</div>'
-    + '</div>'
+var accomodation = '<div class="accomodation">'
+    + ''
     + '</div>';
