0db4f321131c01cc71c3a2040334aaad6a369d4e
Jakob Fogelberg <jakfo369@student.liu.se>
2021-03-12 16:30:50 +0100

    added comments on the code, started working on introducing the class attributes with class ad.

--- server/main.py
+++ server/main.py
@@ -15,11 +15,10 @@ jwt = JWTManager(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-########################### CLASSES ###########################
+###################################################### CLASSES ######################################################
 
-# The class user contains all information about the user. Written by Jakob, Gustav, Joel & Fredrik
-
-
+# The class user contains all information about the user. 
+# Written by Jakob, Gustav, Joel & Fredrik
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -27,9 +26,9 @@ class User(db.Model):
     telephone = db.Column(db.String, nullable=False)
 
     gender = db.Column(db.String, nullable=False)
-    birthdate = db.relationship("Date", backref ='birthdate')
-    hostads = db.relationship("Ad", backref = "host", foreign_keys = "Ad.host_id")
-    bookedads = db.relationship("Ad", backref = "tenant", foreign_keys = "Ad.tenant_id")
+    birthdate = db.relationship("Date", backref='birthdate')
+    hostads = db.relationship("Ad", backref="host", foreign_keys="Ad.host_id")
+    bookedads = db.relationship("Ad", backref="tenant", foreign_keys="Ad.tenant_id")
 
     university = db.Column(db.String, nullable=True)
     education = db.Column(db.String, nullable=True)
@@ -39,13 +38,14 @@ class User(db.Model):
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
     password_hash = db.Column(db.String, nullable=False)
 
-    
-
     def __repr__(self):
-        return '<user {} {} {} {} {} {} {} {} {} {} >'.format(self.id, self.name, self.email, self.telephone, self.gender, self.university, self.education, self.bio, self.verified_student, self.is_admin)
+        return '<user {} {} {} {} {} {} {} {} {} {} >'.format(self.id, self.name, self.email, self.telephone, 
+        self.gender, self.university, self.education, self.bio, self.verified_student, self.is_admin)
 
     def serialize(self):
-        return dict(id=self.id, name=self.name, email=self.email, telephone=self.telephone, gender=self.gender, university=self.university, education=self.education, bio=self.bio, verified_student=self.verified_student, is_admin=self.is_admin, birthdate=Date.query.filter_by(user_id = self.id).first().serialize()) 
+        return dict(id=self.id, name=self.name, email=self.email, telephone=self.telephone, gender=self.gender, 
+        university=self.university, education=self.education, bio=self.bio, verified_student=self.verified_student, 
+        is_admin=self.is_admin, birthdate=Date.query.filter_by(user_id=self.id).first().serialize())
 
     def set_password(self, password):
         self.password_hash = bcrypt.generate_password_hash(
@@ -55,7 +55,9 @@ class User(db.Model):
         self.is_admin = True
 
 
-# The class ad containts all the information about the ads. A ad is owned by a user. Written by Jakob, Gustav, Joel & Fredrik
+# The class ad containts all the information about the ads. An ad is owned by a user. 
+# Written by Jakob, Gustav, Joel
+# What to do: Change nullable from false to true, print attributes
 class Ad(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -69,47 +71,63 @@ class Ad(db.Model):
     postalcode = db.Column(db.Integer, nullable=True)
     country = db.Column(db.String, nullable=True)
 
-    startdate = db.relationship("Date", backref ='start', foreign_keys = "Date.start_ad_id")
-    enddate = db.relationship("Date", backref ='end', foreign_keys = "Date.end_ad_id")
-    #attributes = db.relationship("Attributes", backref = 'ad_attribute')
+    startdate = db.relationship(
+        "Date", backref='start', foreign_keys="Date.start_ad_id")
+    enddate = db.relationship("Date", backref='end',foreign_keys="Date.end_ad_id")
+    attributes = db.relationship("Attributes", backref = 'ad_attribute')
 
     squaremetres = db.Column(db.Integer, nullable=True)
     price = db.Column(db.Integer, nullable=True)
     beds = db.Column(db.Integer, nullable=True)
     accommodationtype = db.Column(db.String, nullable=True)
-   
+
     host_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
     tenant_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
 
     def __repr__(self):
-        return '<Ad {} {} {} {} {} {} {} {} {} {} {} {} {} {}>'.format(self.id, self.title, self.description, self.neighbourhood, self.studentcity, self.streetaddress, self.streetnumber, self.city, self.postalcode, self.country, self.squaremetres, self.price, self.beds, self.accommodationtype)
+        return '<Ad {} {} {} {} {} {} {} {} {} {} {} {} {} {}>'.format(self.id, self.title, self.description, 
+        self.neighbourhood, self.studentcity, self.streetaddress, self.streetnumber, self.city, self.postalcode, 
+        self.country, self.squaremetres, self.price, self.beds, self.accommodationtype)
 
     def serialize(self):
-        return dict(id=self.id, title=self.title, description=self.description, neighbourhood=self.neighbourhood, studentcity=self.studentcity, streetaddress=self.streetaddress, streetnumber=self.streetnumber, city=self.city, postalcode=self.postalcode, country=self.country, squaremetres=self.squaremetres, price=self.price, beds=self.beds, accommodationtype=self.accommodationtype, host=User.query.get(self.host_id).serialize(), startdate=Date.query.filter_by(start_ad_id = self.id).first().serialize(), enddate=Date.query.filter_by(end_ad_id = self.id).first().serialize() )
+        return dict(id=self.id, title=self.title, description=self.description, neighbourhood=self.neighbourhood, 
+        studentcity=self.studentcity, streetaddress=self.streetaddress, streetnumber=self.streetnumber, city=self.city, 
+        postalcode=self.postalcode, country=self.country, squaremetres=self.squaremetres, price=self.price, beds=self.beds, 
+        accommodationtype=self.accommodationtype, host=User.query.get(self.host_id).serialize(), 
+        startdate=Date.query.filter_by(start_ad_id=self.id).first().serialize(), 
+        enddate=Date.query.filter_by(end_ad_id=self.id).first().serialize())
 
+
+#The class attributes contains all the attributes of ad that has a boolean.
+#Written by Jakob, Joel, Gustav
+#What to do: Connect to ad. 
 class Attributes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    dishwasher = db.Column(db.Boolean, nullable=False)
-    wifi = db.Column(db.Boolean, nullable=False)
-    washingmachine = db.Column(db.Boolean, nullable=False)
-    sauna = db.Column(db.Boolean, nullable=False)
-    bike = db.Column(db.Boolean, nullable=False)
-    ad_id = db.Column(db.Integer, db.ForeignKey('ad.id'), nullable = True)
+    dishwasher = db.Column(db.Boolean, nullable=False, default=False)
+    wifi = db.Column(db.Boolean, nullable=False, default=False)
+    washingmachine = db.Column(db.Boolean, nullable=False, default=False)
+    sauna = db.Column(db.Boolean, nullable=False, default=False)
+    bike = db.Column(db.Boolean, nullable=False, default=False)
+    ad_id = db.Column(db.Integer, db.ForeignKey('ad.id'), nullable=True)
 
     def __repr__(self):
-        return '<Attributes {} {} {} {} {} {}>'.format(self.id, self.dishwasher, self.wifi, self.washingmachine, self.sauna, self.bike)
+        return '<Attributes {} {} {} {} {} {}>'.format(self.id, self.dishwasher, self.wifi, 
+        self.washingmachine, self.sauna, self.bike)
 
     def serialize(self):
-        return dict(id=self.id, dishwasher=self.dishwasher, wifi=self.wifi, washingmachine=self.washingmachine, sauna=self.sauna, bike=self.bike)
-    
+        return dict(id=self.id, dishwasher=self.dishwasher, wifi=self.wifi, 
+        washingmachine=self.washingmachine, sauna=self.sauna, bike=self.bike)
+
+#The class date is used for managing dates. Both ad and user uses the date class. 
+#Written by Jakob, Gustav, Joel
 class Date(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer, nullable=False)
     month = db.Column(db.Integer, nullable=False)
     day = db.Column(db.Integer, nullable=False)
-    start_ad_id = db.Column(db.Integer, db.ForeignKey('ad.id'), nullable = True)
-    end_ad_id = db.Column(db.Integer, db.ForeignKey('ad.id'), nullable = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = True)
+    start_ad_id = db.Column(db.Integer, db.ForeignKey('ad.id'), nullable=True)
+    end_ad_id = db.Column(db.Integer, db.ForeignKey('ad.id'), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
 
     def __repr__(self):
         return '<Date {} {} {} {}>'.format(self.id, self.year, self.month, self.day)
@@ -118,35 +136,34 @@ class Date(db.Model):
         return dict(id=self.id, year=self.year, month=self.month, day=self.day)
 
 
+###################################################### APP.ROUTES ######################################################
 
-########################### APP.ROUTES ###########################
-
-# /user/signup has the method POST that is used when you want to create a new user on the website. Written by Jakob, Gustav & Joel
-
-
+# /user/signup has the method POST that is used when you want to create a new user on the website. 
+# Written by Jakob, Gustav
+# KIND OF DONE
 @app.route('/user/signup', methods=['POST'])
 def signup():
     newuser = request.get_json(force=True)
     if not (User.query.filter_by(email=newuser.get('email')).first()):
-        newuserDB = User(name=newuser.get('name'), email=newuser.get('email'), telephone=newuser.get('telephone'), gender=newuser.get('gender'))
+        newuserDB = User(name=newuser.get('name'), email=newuser.get(
+            'email'), telephone=newuser.get('telephone'), gender=newuser.get('gender'))
         User.set_password(newuserDB, newuser.get('password'))
         db.session.add(newuserDB)
         db.session.commit()
-        birthdateDB = Date(year=newuser.get('year'), month=newuser.get('month'), day=newuser.get('day'), user_id = newuserDB.id)
+        birthdateDB = Date(year=newuser.get('year'), month=newuser.get(
+            'month'), day=newuser.get('day'), user_id=newuserDB.id)
         db.session.add(birthdateDB)
         db.session.commit()
         return "Created", 201
     else:
         return "E-mail already in use", 409
-    # if(newuser.get('is_admin') == "True"):
-    #    User.set_admin(newuserDB)
-    # Needs to be changed
-#KLAR TILLS VIDARE
-    
-
-# /user/login has the method POST that is used when you want to log in with a user. Written by Jakob, Gustav, Joel & Fredrik
 
 
+
+
+# /user/login has the method POST that is used when you want to log in with a user. 
+# Written by Jakob, Gustav, Joel
+# KIND OF DONE
 @app.route('/user/login', methods=['POST'])
 def login():
     user = request.get_json(force=True)
@@ -161,9 +178,11 @@ def login():
             abort(401)
     else:
         abort(401)
-#KLAR TILLS VIDARE
 
-# /users has the method GET that is used when you want to retrieve all the users that are in the database. Not sure if we actually need it. Written by Jakob, Gustav, Joel & Fredrik
+
+# /users has the method GET that is used when you want to retrieve all the users that are in the database. Not sure if we actually need it. 
+# Written by Jakob, Gustav, Joel
+# KIND OF DONE
 @app.route('/users', methods=['GET'])
 def list_users():
     user_list = []
@@ -171,9 +190,10 @@ def list_users():
     for user in all_users:
         user_list.append(user.serialize())
     return jsonify(user_list)
-#KLAR TILLS VIDARE
+
 
 # /ad/<int:ad_id> has the method PUT, GET. The method PUT .....
+#Written by Jakob, Joel, Gustav
 @app.route('/ad/<int:ad_id>', methods=['PUT', 'GET'])
 def list_ad(ad_id):
     if request.method == 'GET':
@@ -182,7 +202,8 @@ def list_ad(ad_id):
         return "NYI"
 
 
-# /ads has the method GET, it is used to retrieve all the ads that is stored in the database. Written by Jakob, Gustav, Joel & Fredrik
+# /ads has the method GET, it is used to retrieve all the ads that is stored in the database. 
+# Written by Jakob, Gustav, Joel & Fredrik
 @app.route('/ads', methods=['GET'])
 def ads():
     if request.method == 'GET':
@@ -193,7 +214,8 @@ def ads():
         return jsonify(ad_list)
 
 
-# /ad/create has the method POST, it is used to make an ad that is connected to the specifik user that created it. Written by Jakob, Gustav, Joel & Fredrik
+# /ad/create has the method POST, it is used to make an ad that is connected to the specifik user that created it. 
+# Written by Jakob, Gustav, Joel & Fredrik
 @app.route('/ad/create', methods=['POST'])
 @jwt_required()
 def create_ad():
@@ -204,10 +226,14 @@ def create_ad():
             'description'), host_id=(current_user_id))
         db.session.add(newadDB)
         db.session.commit()
-        startdayDB = Date(year=newad.get('startyear'), month=newad.get('startmonth'), day=newad.get('startday'), start_ad_id = newadDB.id)
-        enddayDB = Date(year=newad.get('endyear'), month=newad.get('endmonth'), day=newad.get('endday'), end_ad_id = newadDB.id)
+        startdayDB = Date(year=newad.get('startyear'), month=newad.get(
+            'startmonth'), day=newad.get('startday'), start_ad_id=newadDB.id)
+        enddayDB = Date(year=newad.get('endyear'), month=newad.get(
+            'endmonth'), day=newad.get('endday'), end_ad_id=newadDB.id)
+        attributesDB = Attributes(ad_id=newadDB.id)
         db.session.add(startdayDB)
         db.session.add(enddayDB)
+        db.session.add(attributesDB)
         db.session.commit()
 
         return "success", 200
