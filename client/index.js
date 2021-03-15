d44f66fc66502b5c7485cd3aeb276973074469b1
albinjanzen <albinjanzen@gmail.com>
2021-03-15 15:22:46 +0100

    preparations for merge from gustav/backend branch - function/request for loading all ads from database

--- client/index.js
+++ client/index.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
-    $("#content").html($("#home").html());
+    $("#content").html($("#search_page").html());
     $("#header").html($("#home_header").html());
+    load_ads();
 })
+
+function load_ads() {
+    $.ajax({
+        url: host + '/ads',
+        type: 'GET',
+        success: function (ads) {
+            ads.array.forEach(element => {
+                $("#seach_result").append(Mustache.render(accomodation, element.title));
+            });
+        }
+    })
+}
