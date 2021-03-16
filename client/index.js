77455c2ca524cc4452e2aa75c0691da45bf7bdea
albinjanzen <albinjanzen@gmail.com>
2021-03-16 10:27:38 +0100

    fixed menu position, added view for login, added view for register, added forms to register and login views, added minor design and responsiveness to forms, created function for loading years in register form, added describing comments

--- client/index.js
+++ client/index.js
@@ -1,16 +1,51 @@
 $(document).ready(function () {
-    $("#content").html($("#search_page").html());
+    $("#content").html($("#register_page").html());
     $("#header").html($("#home_header").html());
     load_ads();
+    load_years();
+    load_months();
+    load_days();
 })
 
+
+
+//Function for loading in all selectable years in register form
+function load_years() {
+    var year = new Date().getFullYear();
+    for (i = 0; i < 100; i++) {
+        $("#year_register").append("<option>" + year + "</option>")
+        --year;
+    }
+}
+
+//Function for loading in all selectable months in register form
+function load_months() {
+    for (i = 1; i < 13; i++) {
+        $("#month_register").append("<option>" + i + "</option>")
+    }
+}
+
+//Function for loading in all selectable days in register form
+function load_days() {
+    for (i = 1; i < 32; i++) {
+        $("#day_register").append("<option>" + i + "</option>")
+    }
+}
+
+//Global request variable: host
+var host = 'http://localhost:5000';
+
+//Function for making a request for all ads from database
 function load_ads() {
     $.ajax({
         url: host + '/ads',
         type: 'GET',
         success: function (ads) {
-            ads.array.forEach(element => {
-                $("#seach_result").append(Mustache.render(accomodation, element.title));
+            console.log(ads);
+            $("#search_result").empty();
+            ads.forEach(element => {
+                console.log(element.title);
+                $("#search_result").append(Mustache.render(accomodation, element));
             });
         }
     })
--- client/templates.js
+++ client/templates.js
@@ -1,3 +1,3 @@
 var accomodation = '<div class="accomodation">'
-    + '{{Title}}'
+    + '{{title}}'
     + '</div>';
